cedd79707fcced624b0b76dd949c34c67b9c909b
Maes95 <michel.maes95@gmail.com>
2023-02-14 15:36:09 +0100

    Set up template

---
+++ src/test/java/es/codeurjc/ais/e2e/selenium/SeleniumTest.java
@@ -0,0 +1,59 @@
+package es.codeurjc.ais.e2e.selenium;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SeleniumTest {
+
+    @LocalServerPort
+    int port;
+
+    private WebDriver driver;
+
+	@BeforeAll
+	public static void setupClass() {
+		WebDriverManager.chromedriver().setup();
+	}
+
+	@BeforeEach
+	public void setupTest() {
+        this.driver = new ChromeDriver();
+	}
+
+	@AfterEach
+	public void teardown() {
+		if (this.driver != null) {
+            this.driver.quit();
+		}
+    }
+    
+    @Test
+    @DisplayName("Check that the default topic is fantasy")
+	public void checkDefaultTopic() throws Exception {
+
+        String topic = "fantasy";
+
+        // GIVEN: Partiendo de que estamos en la página principa
+        this.driver.get("http://localhost:"+this.port+"/");
+
+        // WHEN: Introducimos drama en el campo de búsqueda y pulsamos el botón de buscar
+        String title = driver.findElement(By.tagName("h1")).getText();
+        
+        // THEN: Comprobamos que el título de la página es "Books (topic=fantasy)"
+        assertEquals("Books (topic="+topic+")", title);
+    }
+    
+}
